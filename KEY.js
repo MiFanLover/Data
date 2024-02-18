e5a1cc45a33a084375417dbc07141b933226ee9e
MiFanLover <leijun@jinfan.com>
2024-02-18 21:20:24 +0800

    update

---
+++ KEY.js
@@ -0,0 +1,34 @@
+Welcome to Ubuntu 20.04.4 LTS (GNU/Linux 5.15.0-1040-oracle x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+ * Strictly confined Kubernetes makes edge and IoT secure. Learn how MicroK8s
+   just raised the bar for easy, resilient and secure K8s cluster deployment.
+
+   https://ubuntu.com/engage/secure-kubernetes-at-the-edge
+
+This system has been minimized by removing packages and content that are
+not required on a system that users do not log into.
+
+To restore this content, you can run the 'unminimize' command.
+
+74 updates can be applied immediately.
+To see these additional updates run: apt list --upgradable
+
+New release '22.04.3 LTS' available.
+Run 'do-release-upgrade' to upgrade to it.
+
+*** System restart required ***
+timestamp,price
+1701330446,27.99
+1708224800,65.0
+1707788387,50.0
+1707153277,61.99
+1706351695,61.99
+1706263745,55.0
+1705978254,38.5
+1702553926,27.99
+1701330446,27.99
+1708260784,65.0
