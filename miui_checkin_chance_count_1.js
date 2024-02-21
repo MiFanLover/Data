0c563c35f3cb3e3d3710dd2b6cd9ccddd1df10ef
MiFanLover <leijun@jinfan.com>
2024-02-21 17:20:15 +0800

    update

---
+++ miui_checkin_chance_count_1.js
@@ -0,0 +1,1 @@
+{"code":200,"updatedAt":1708507201521,"data":[{"count":62084,"timestamp":1708498800,"type":"miui_checkin_chance_count_1"},{"count":45643,"timestamp":1708502400,"type":"miui_checkin_chance_count_1"},{"count":62740,"timestamp":1708506000,"type":"miui_checkin_chance_count_1"}]}
